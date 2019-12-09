1a0a368e693a58560e389407865f342c1a064a7e
胡皓 <hao.hu@changhong.com>
2019-12-09 09:46:16 +0800

    delete template

--- template/src/api/index.js
+++
@@ -1,8 +0,0 @@
-import Http from "@/utils/http";
-const url = "http://wthrcdn.etouch.cn/weather_mini";
-export default {
-  // 获取天气
-  getWeather: params => {
-    return Http.get(url, params);
-  }
-};
